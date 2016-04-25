9800114916013e98d083860f30480e3981448f52
Karl Hepler <khepler@brandmovers.com>
2016-04-25 17:53:41 -0400

    cache busting for fb image

--- source/resources/views/index.blade.php
+++ source/resources/views/index.blade.php
@@ -14,7 +14,7 @@
     <meta property="og:type" content="article" />
     <meta property="og:title" content="Robin Kessinger Festival & Workshops" />
     <meta property="og:description" content="Home of the west virginia state flatpick guitar contest & part of heritage farm museum and village's 'way back' music weekend." />
-    <meta property="og:image" content="http://www.robinkessingerfestival.org/images/fb.png" />
+    <meta property="og:image" content="http://www.robinkessingerfestival.org/images/fb.png?v=1" />
     <link rel="stylesheet" href="/app.css">
     <script src="//cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js"></script>
 </head>
