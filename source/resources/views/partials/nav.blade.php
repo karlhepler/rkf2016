c9a047118cd78dc56853bd6b423c2460327c8038
Karl Hepler <khepler@brandmovers.com>
2016-04-26 11:00:46 -0400

    updated nav links

--- source/resources/views/partials/nav.blade.php
+++ source/resources/views/partials/nav.blade.php
@@ -13,13 +13,13 @@
     ps-size="@{{ isMobile() ? '80vw' : '50vw' }}">
     <div>
         <nav class="Navigation">
-            <a class="Navigation__link" href="#" ng-click="showMenu = false" scroll-to="about-festival">About the Festival</a>
-            <a class="Navigation__link" href="#" ng-click="showMenu = false" scroll-to="performers-instructors">Performers &amp; Instructors</a>
-            <a class="Navigation__link" href="#" ng-click="showMenu = false" scroll-to="location-directions">Location &amp; Directions</a>
-            <a class="Navigation__link" href="#" ng-click="showMenu = false" scroll-to="schedule-workshops">Schedule &amp; Workshops</a>
-            <a class="Navigation__link" href="#" ng-click="showMenu = false" scroll-to="contest-information">Contest Information</a>
-            <a class="Navigation__link" href="#" ng-click="showMenu = false" scroll-to="tickets-admission">Tickets &amp; Admission</a>
-            <a class="Navigation__link" href="#" ng-click="showMenu = false" scroll-to="contact-us">Contact Us</a>
+            <a class="Navigation__link" href="#about-festival" ng-click="showMenu = false" scroll-to="about-festival">About the Festival</a>
+            <a class="Navigation__link" href="#performers-instructors" ng-click="showMenu = false" scroll-to="performers-instructors">Performers &amp; Instructors</a>
+            <a class="Navigation__link" href="#location-directions" ng-click="showMenu = false" scroll-to="location-directions">Location &amp; Directions</a>
+            <a class="Navigation__link" href="#schedule-workshops" ng-click="showMenu = false" scroll-to="schedule-workshops">Schedule &amp; Workshops</a>
+            <a class="Navigation__link" href="#contest-information" ng-click="showMenu = false" scroll-to="contest-information">Contest Information</a>
+            <a class="Navigation__link" href="#tickets-admission" ng-click="showMenu = false" scroll-to="tickets-admission">Tickets &amp; Admission</a>
+            <a class="Navigation__link" href="#contact-us" ng-click="showMenu = false" scroll-to="contact-us">Contact Us</a>
         </nav>
     </div>
 </pageslide>
