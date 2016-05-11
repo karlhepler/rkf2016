4d9d828d7ded2f8c55fc0de6f9be45ad8b2941d7
Karl Hepler <khepler@brandmovers.com>
2016-05-11 15:14:06 -0400

    Added custom mailer class

--- source/app/Console/Commands/TestEmail.php
+++ source/app/Console/Commands/TestEmail.php
@@ -3,28 +3,18 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use Illuminate\Contracts\Mail\Mailer;
 
 class TestEmail extends Command
 {
-    protected $signature = 'test:email
-                            {--to= : The email address to which you will send the test email}';
+    protected $signature = 'test:email {--to= : The email address to which you will send the test email}';
     protected $description = 'Test sending an email to make sure emailing works';
-    protected $mailer;
-
-    function __construct(Mailer $mailer)
-    {
-        parent::__construct();
-
-        $this->mailer = $mailer;
-    }
 
     public function handle()
     {
         $this->enforceRequiredOptions();
 
         $this->info('Sending...');
-        $this->sendTestEmailTo($this->option('to'));
+        email()->test($this->option('to'));
         $this->info('Sent to ' . $this->option('to') . '!');
     }
 
@@ -41,20 +31,5 @@ class TestEmail extends Command
             $this->error('You must supply a "to" email address: --to=email@address.com');
             die;
         }
-    }
-
-    /**
-     * Send a test email to the given email address
-     *
-     * @param  string $emailAddress
-     * @return void
-     */
-    protected function sendTestEmailTo($emailAddress)
-    {
-        $this->mailer->raw('Email for RKF works!',
-        function($message) use ($emailAddress) {
-            $message->to($emailAddress);
-            $message->subject('Email from RKF works!');
-        });
     }
 }
---
+++ source/app/RobinKessingerFestival/Mailer.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\RobinKessingerFestival;
+
+use Illuminate\Contracts\Mail\Mailer as IlluminateMailer;
+
+class Mailer
+{
+    /**
+     * Illuminate Mailer
+     *
+     * @var \Illuminate\Contracts\Mail\Mailer
+     */
+    protected $mailer;
+
+    /**
+     * Consolidated mailer class that also
+     * defines a global email() function
+     *
+     * @param Mailer $mailer
+     */
+    public function __construct(IlluminateMailer $mailer)
+    {
+        $this->mailer = $mailer;
+
+        $this->registerGlobalEmailFunction();
+    }
+
+    ////////////////////
+    // PUBLIC METHODS //
+    ////////////////////
+
+    /**
+     * Send a test email to the given email address
+     *
+     * @return void
+     */
+    public function mailTest($emailAddress)
+    {
+        $this->mailer->raw('Email for RKF works!',
+        function($message) use ($emailAddress) {
+            $message->to($emailAddress);
+            $message->subject('Email from RKF works!');
+        });
+    }
+
+    ///////////////////////
+    // PROTECTED METHODS //
+    ///////////////////////
+
+    /**
+     * Register the global email() function
+     *
+     * @return void
+     */
+    protected function registerGlobalEmailFunction()
+    {
+        function email()
+        {
+            return $this;
+        }
+    }
+
+    ///////////////////
+    // MAGIC METHODS //
+    ///////////////////
+
+    /**
+     * Handle calling mail methods
+     *
+     * @param  string $name
+     * @param  array  $args
+     * @return mixed
+     */
+    public function __call($name, array $args)
+    {
+        // Get the method name
+        $methodName = 'mail'.ucfirst(strtolower($name));
+
+        // If the method doesn't exist, throw an exception
+        if ( !method_exists($this, $methodName) ) {
+            throw new \BadMethodCallException("Neither {$name} nor {$methodName} are valid methods in " . get_class($this));
+        }
+
+        // Call the method!
+        return call_user_func_array([$this, $methodName], $args);
+    }
+}
