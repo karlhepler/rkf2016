7b1b61c09deb5b87feb8361903001eb77d27f23e
Karl Hepler <khepler@brandmovers.com>
2016-05-31 22:10:19 -0400

    Updated admission information

--- source/resources/views/partials/tickets-admission.blade.php
+++ source/resources/views/partials/tickets-admission.blade.php
@@ -1,9 +1,18 @@
 <section id="tickets-admission" class="grid-container">
     <h3>Tickets &amp; Admission</h3>
-    <h4 class="blue">$15 or $25 Weekend</h4>
-    <br><br>
-    <p class="align-center red uppercase"><strong>12 &amp; under FREE/accompanied by an adult</strong></p>
-    <p class="align-center red uppercase"><strong>No refunds</strong></p>
-    <p class="align-center red uppercase"><strong>No Alcohol</strong></p>
-    <p class="align-center red uppercase"><strong>Limited Concessions</strong></p>
+    <h4 class="align-left blue">Friday</h4>
+    <p>Friday evening event $15 per person (12 and under free)</p>
+    <br>
+    <h4 class="align-left blue">Saturday</h4>
+    <p>
+        Saturday admission is Adults &mdash; $15, Ages 3-18 &mdash; $10, 2 and Under are FREE.<br>
+        Saturday admission includes entry into Smithsonian-affiliated museums, petting zoo, wagon ride, Discovery Zone
+        and MakerSpace in addition to all RKF music events. The WayBack Weekend passes are valid for all of Saturday's events.
+        Please refer to Heritage Farm Museum and Village link for more information.
+    </p>
+    <p class="align-center red uppercase">
+        <strong>No refunds</strong> &bull;
+        <strong>No Alcohol</strong> &bull;
+        <strong>Limited Concessions</strong>
+    </p>
 </section>
