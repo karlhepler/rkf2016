6234acafbcd5959e6c0634ff2c61a104547b4f48
Karl Hepler <khepler@brandmovers.com>
2018-01-17 16:10:59 -0500

    changed og links to http

--- source/resources/views/tags/fb.blade.php
+++ source/resources/views/tags/fb.blade.php
@@ -1,5 +1,5 @@
-<meta property="og:url" content="http://www.robinkessingerfestival.org" />
+<meta property="og:url" content="https://robinkessingerfestival.org" />
 <meta property="og:type" content="article" />
 <meta property="og:title" content="Robin Kessinger West VA State Flatpick Guitar Contest & Workshops" />
 <meta property="og:description" content="Proudly partnered with the Fly In Festival, Aug 23-25 2018" />
-<meta property="og:image" content="http://www.robinkessingerfestival.org/images/fb.png?v=2" />
+<meta property="og:image" content="https://robinkessingerfestival.org/images/fb.png?v=2" />
