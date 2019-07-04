b523cd5ba7fcdc65361ee5fbbbe54dc8a8270c30
Karl Hepler <karl.hepler@bronto.com>
2019-07-04 19:36:42 -0400

    updated facebook image for 2019

--- source/resources/views/tags/fb.blade.php
+++ source/resources/views/tags/fb.blade.php
@@ -2,4 +2,4 @@
 <meta property="og:type" content="article" />
 <meta property="og:title" content="Robin Kessinger West VA State Flatpick Guitar Contest & Workshops" />
 <meta property="og:description" content="Proudly partnered with the Fly In Festival, Aug 23-24 2019" />
-<meta property="og:image" content="https://robinkessingerfestival.org/images/fb.png?v=2" />
+<meta property="og:image" content="https://robinkessingerfestival.org/images/fb.png?2019" />
